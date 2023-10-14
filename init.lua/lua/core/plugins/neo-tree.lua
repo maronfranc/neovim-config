3ae0a5293bdf44b90c132eba45c086a60f4b1615
Máron <maron.franca@gmail.com>
2023-10-13 21:05:55 -0300

    [plugins] neo-tree delete confirmation

--- init.lua/lua/core/plugins/neo-tree.lua
+++ init.lua/lua/core/plugins/neo-tree.lua
@@ -1,5 +1,5 @@
 -- @see https://github.com/nvim-neo-tree/neo-tree.nvim
--- Neo-tree is a Neovim plugin to browse the file system and other tree like structures 
+-- Neo-tree is a Neovim plugin to browse the file system and other tree like structures
 -- in whatever style suits you, including sidebars, floating windows, netrw split style,
 -- or all of them at once.
 local icons = require("core.utils.icons")
@@ -20,7 +20,7 @@ local M = {
     -- The name used here must be the same name you would use in a require() call.
     sources = { "filesystem", "buffers", "git_status" },
     add_blank_line_at_top = false, -- Add a blank line at the top of the tree.
-    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+    close_if_last_window = true,   -- Close Neo-tree if it is the last window left in the tab
     -- popup_border_style is for input and confirmation dialogs.
     -- Configurtaion of floating window is done in the individual source sections.
     -- "NC" is a special style that works well with NormalNC set
@@ -34,31 +34,32 @@ local M = {
     -- These options are for people with VERY large git repos
     git_status_async_options = {
       batch_size = 1000, -- how many lines of git status results to process at a time
-      batch_delay = 10, -- delay in ms between batches. Spreads out the workload to let other processes run.
+      batch_delay = 10,  -- delay in ms between batches. Spreads out the workload to let other processes run.
       max_lines = 10000, -- How many lines of git status results to process. Anything after this will be dropped.
       -- Anything before this will be used. The last items to be processed are the untracked files.
     },
-    hide_root_node = false, -- Hide the root node.
+    hide_root_node = false,            -- Hide the root node.
     retain_hidden_root_indent = false, -- IF the root node is hidden, keep the indentation anyhow.
     -- This is needed if you use expanders because they render in the indent.
-    log_level = "info", -- "trace", "debug", "info", "warn", "error", "fatal"
-    log_to_file = false, -- true, false, "/path/to/file.log", use :NeoTreeLogs to show the file
-    open_files_in_last_window = true, -- false = open files in top left window
-    popup_border_style = "NC", -- "double", "none", "rounded", "shadow", "single" or "solid"
-    resize_timer_interval = 500, -- in ms, needed for containers to redraw right aligned and faded content
+    log_level = "info",                -- "trace", "debug", "info", "warn", "error", "fatal"
+    log_to_file = false,               -- true, false, "/path/to/file.log", use :NeoTreeLogs to show the file
+    open_files_in_last_window = true,  -- false = open files in top left window
+    popup_border_style = "NC",         -- "double", "none", "rounded", "shadow", "single" or "solid"
+    resize_timer_interval = 500,       -- in ms, needed for containers to redraw right aligned and faded content
     -- set to -1 to disable the resize timer entirely
     -- NOTE: this will speed up to 50 ms for 1 second following a resize
     sort_case_insensitive = false, -- used when sorting files and directories in the tree
-    sort_function = nil, -- uses a custom function for sorting files and directories in the tree
-    use_popups_for_input = true, -- If false, inputs will use vim.ui.input() instead of custom floats.
+    sort_function = nil,           -- uses a custom function for sorting files and directories in the tree
+    use_popups_for_input = true,   -- If false, inputs will use vim.ui.input() instead of custom floats.
     use_default_mappings = true,
     -- source_selector provides clickable tabs to switch between sources.
     source_selector = {
-      winbar = true, -- toggle to show selector on winbar
-      statusline = false, -- toggle to show selector on statusline
+      winbar = true,                         -- toggle to show selector on winbar
+      statusline = false,                    -- toggle to show selector on statusline
       show_scrolled_off_parent_node = false, -- this will replace the tabs with the parent path
       -- of the top visible node when scrolled down.
-      tab_labels = { -- falls back to source_name if nil
+      tab_labels = {
+        -- falls back to source_name if nil
         filesystem = " Files ",
         buffers = " Buffers ",
         git_status = " Git ",
@@ -75,9 +76,9 @@ local M = {
       --             equal  : |/    a    \/    b    \/    c    \|
       --             active : |/  focused tab    \/  b  \/  c  \|
       truncation_character = "…", -- character to use when truncating the tab label
-      tabs_min_width = nil, -- nil | int: if int padding is added based on `content_layout`
-      tabs_max_width = nil, -- this will truncate text even if `text_trunc_to_fit = false`
-      padding = 0, -- can be int or table
+      tabs_min_width = nil,       -- nil | int: if int padding is added based on `content_layout`
+      tabs_max_width = nil,       -- this will truncate text even if `text_trunc_to_fit = false`
+      padding = 0,                -- can be int or table
       -- padding = { left = 2, right = 0 },
       -- separator = "▕", -- can be string or table, see below
       separator = { left = "▏", right = "▕" },
@@ -160,13 +161,13 @@ local M = {
         {
           "container",
           content = {
-            { "name", zindex = 10 },
+            { "name",        zindex = 10 },
             -- {
             --   "symlink_target",
             --   zindex = 10,
             --   highlight = "NeoTreeSymbolicLinkTarget",
             -- },
-            { "clipboard", zindex = 10 },
+            { "clipboard",   zindex = 10 },
             { "diagnostics", errors_only = true, zindex = 20, align = "right" },
           },
         },
@@ -177,29 +178,31 @@ local M = {
         {
           "container",
           content = {
-            { "name", zindex = 10 },
-            -- { "symlink_target", zindex = 10, highlight = "NeoTreeSymbolicLinkTarget" },
-            { "clipboard", zindex = 10 },
-            { "bufnr", zindex = 10 },
-            { "modified", zindex = 20, align = "right" },
+            { "name",        zindex = 10 },
+            { "clipboard",   zindex = 10 },
+            { "bufnr",       zindex = 10 },
+            { "modified",    zindex = 20, align = "right" },
             { "diagnostics", zindex = 20, align = "right" },
-            { "git_status", zindex = 20, align = "right" },
+            { "git_status",  zindex = 20, align = "right" },
+            -- { "symlink_target", zindex = 10, highlight = "NeoTreeSymbolicLinkTarget" },
           },
         },
       },
       message = {
         { "indent", with_markers = false },
-        { "name", highlight = "NeoTreeMessage" },
+        { "name",   highlight = "NeoTreeMessage" },
       },
       terminal = { { "indent" }, { "icon" }, { "name" }, { "bufnr" } },
     },
     nesting_rules = {},
-    window = { -- see https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup for
+    window = {
+      -- see https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup for
       -- possible options. These can also be functions that return these options.
       position = "left", -- left, right, top, bottom, float, current
-      width = 30, -- applies to left and right positions
-      height = 15, -- applies to top and bottom positions
-      popup = { -- settings that apply to float position only
+      width = 30,        -- applies to left and right positions
+      height = 15,       -- applies to top and bottom positions
+      popup = {
+        -- settings that apply to float position only
         size = { height = "80%", width = "50%" },
         position = "50%", -- 50% means center it
         -- you can also specify border here, if you want a different setting from
@@ -251,9 +254,13 @@ local M = {
         -- Override delete to use trash instead of rm
         delete = function(state)
           local path = state.tree:get_node().path
-          print("TODO: implement delete prompt confirmation: ", path)
-          -- vim.fn.system({ "trash", vim.fn.fnameescape(path) })
-          -- require("neo-tree.sources.manager").refresh(state.name)
+          local msg = "Are you sure you want to delete" .. path
+          local inputs = require "neo-tree.ui.inputs"
+          inputs.confirm(msg, function(confirmed)
+            if not confirmed then return end
+            vim.fn.system { "trash-put", vim.fn.fnameescape(path) }
+            require("neo-tree.sources.manager").refresh(state.name)
+          end)
         end,
         system_open = function(state)
           local node = state.tree:get_node()
@@ -271,22 +278,23 @@ local M = {
           ["H"] = "toggle_hidden",
           ["/"] = "fuzzy_finder",
           ["D"] = "fuzzy_finder_directory",
-          --["/"] = "filter_as_you_type", -- this was the default until v1.28
           ["f"] = "filter_on_submit",
-          ["<C-x>"] = "clear_filter",
-          -- ["<bs>"] = "navigate_up",
           ["."] = "set_root",
           ["[g"] = "prev_git_modified",
           ["]g"] = "next_git_modified",
+          ["<C-x>"] = "clear_filter",
+          -- ["/"] = "filter_as_you_type", -- this was the default until v1.28
+          -- ["<BS>"] = "navigate_up",
         },
       },
-      async_directory_scan = "auto", -- "auto"   means refreshes are async, but it's synchronous when called from the Neotree commands.
+      async_directory_scan = "auto",
+      -- "auto"   means refreshes are async, but it's synchronous when called from the Neotree commands.
       -- "always" means directory scans are always async.
       -- "never"  means directory scans are never async.
       bind_to_cwd = true, -- true creates a 2-way binding between vim's cwd and neo-tree's root
       cwd_target = {
-        sidebar = "tab", -- sidebar is when position = left or right
-        current = "window", -- current is when position = current
+        sidebar = "tab",              -- sidebar is when position = left or right
+        current = "window",           -- current is when position = current
       },
       -- The renderer section provides the renderers that will be used to render the tree.
       --   The first level is the node type.
@@ -295,9 +303,9 @@ local M = {
       --         The first field in each component is the name of the function to call.
       --         The rest of the fields are passed to the function as the "config" argument.
       filtered_items = {
-        visible = false, -- when true, they will just be displayed differently than normal items
+        visible = false,                       -- when true, they will just be displayed differently than normal items
         force_visible_in_empty_folder = false, -- when true, hidden files will be shown if the root folder is otherwise empty
-        show_hidden_count = true, -- when true, the number of hidden items in each folder will be shown as the last entry
+        show_hidden_count = true,              -- when true, the number of hidden items in each folder will be shown as the last entry
         hide_dotfiles = false,
         hide_gitignored = false,
         hide_hidden = true, -- only works on Windows for hidden files/directories
@@ -321,7 +329,7 @@ local M = {
       -- `fi init`
       -- will match: `./sources/filesystem/init.lua
       --find_command = "fd", -- this is determined automatically, you probably don't need to set it
-      find_args = { -- you can specify extra args to pass to the find command.
+      find_args               = { -- you can specify extra args to pass to the find command.
         fd = { "--exclude", ".git", "--exclude", "node_modules" },
       },
       ---- or use a function instead of list of strings
@@ -345,20 +353,20 @@ local M = {
       --  end
       --  return args
       --end,
-      group_empty_dirs = false, -- when true, empty folders will be grouped together
-      search_limit = 50, -- max number of search results when using filters
-      follow_current_file = false, -- This will find and focus the file in the active buffer every time
+      group_empty_dirs        = false, -- when true, empty folders will be grouped together
+      search_limit            = 50,    -- max number of search results when using filters
+      follow_current_file     = false, -- This will find and focus the file in the active buffer every time
       -- "disabled", netrw disabled, opening a directory opens neo-tree in whatever position is specified in window.position
       -- netrw disabled, opening a directory opens within the window like netrw would, regardless of window.position
-      hijack_netrw_behavior  = "open_default", -- "open_current",
-      use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+      hijack_netrw_behavior   = "open_default", -- "open_current",
+      use_libuv_file_watcher  = false,          -- This will use the OS level file watchers to detect changes
       -- instead of relying on nvim autocmd events.
     },
     buffers = {
       bind_to_cwd = true,
       follow_current_file = true, -- This will find and focus the file in the active buffer every time
       -- the current file is changed while the tree is open.
-      group_empty_dirs = true, -- when true, empty directories will be grouped together
+      group_empty_dirs = true,    -- when true, empty directories will be grouped together
       window = {
         mappings = {
           -- ["<bs>"] = "navigate_up",
@@ -384,7 +392,7 @@ local M = {
       renderers = {
         custom = {
           { "indent" },
-          { "icon", default = "C" },
+          { "icon",  default = "C" },
           { "custom" },
           { "name" },
         },
