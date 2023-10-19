541481e5b909c21fa0a82bd9cf54063a89ddaf88
Máron <maron.franca@gmail.com>
2023-10-19 17:45:21 -0300

    [plugin] add start page

--- init.lua/lua/core/plugins/theme.lua
+++ init.lua/lua/core/plugins/theme.lua
@@ -4,9 +4,8 @@ local M = {
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd([[colorscheme midnight]])
-    vim.cmd([[silent! :set bg=dark]])
+    vim.cmd.colorscheme("midnight") -- vim.cmd([[colorscheme midnight]])
+    vim.opt.bg="dark"               -- vim.cmd([[silent! :set bg=dark]])
   end,
 }
-
 return M
---
+++ init.lua/lua/core/plugins/vim-startify.lua
@@ -0,0 +1,5 @@
+-- @see https://github.com/mhinz/vim-startify
+local M = {
+  "mhinz/vim-startify"
+}
+return M
